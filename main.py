06bc186ccbd41f8bb08f3a0221e17c904c92a1c1
joaoSalgueiro <js.lage@gmail.com>
2019-03-19 23:43:37 -0300

    Refactor logic

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-def main(array):
-    answer = False
-    if array[1] == '^':
-        answer = array[0] and array[2]
-    if array[1] == 'v':
-        answer = array[0] or array[2]
-    if array[1] == '~':
-        answer = not array[0]
-    print (answer)
+A = True
+B = False
+C = False
+
+def main():
+    logic = input('Digite a logica: ')
+    logic = str.replace(logic, "v", " or ")
+    logic = str.replace(logic, "^", " and ")
+    logic = str.replace(logic, "~", " not ")
+    return eval(logic)
 
 if __name__ == "__main__":
-    A = True
-    B = False
-    C = True
-    array = [A, '^', B]
-    main(array)
+    print(main())
